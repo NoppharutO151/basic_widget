cb84da82e654b1a4626ed5bfc1ac2f7d304f282d
Noppharut151 <noppharutonlamoon@gmail.com>
2026-01-08 10:26:26 +0700

    Widget Tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +10,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Basic Widgets'),
+          backgroundColor: Colors.deepPurpleAccent,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 24,),
+        ),
+
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/ass1.gif',
+                width: 200,
+                height: 200,
+                
+              ),
+              // --------------------------------------------------------
+
+              const SizedBox(height: 20), 
+              const Text('Hello World!'),
+            ],
+          ),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Text('Click Me'),
+          backgroundColor: Colors.orangeAccent,
+          foregroundColor: Colors.white,
         ),
       ),
     );
